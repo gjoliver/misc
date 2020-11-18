82419110a66ecad748c4f6a53704e2efcc019223
Jun Gong <jungong@mbpro.local>
2020-11-17 21:01:31 -0800

    Add epoxy volume calculation

--- river_table/design.py
+++ river_table/design.py
@@ -1,6 +1,10 @@
 from matplotlib import pyplot as plt
 import numpy
 
+HEIGHT = 36.0  # inches
+THICKNESS = 1.3  # inches
+BLOCK_WIDTH = 1.5  # inches
+
 image = plt.imread('/Users/jungong/Downloads/sound_wav_small.png')
 wav = image[:, :, 0]
 
@@ -10,10 +14,18 @@ numpy.random.shuffle(bars)
 table = numpy.tile(bars, [25, 1])
 
 # Build sound wave river.
+total_river_blocks = 0
 for i in range(25):
   for j in range(56):
     if wav[i, j] == 0.0:
       table[i, j] = 0.0
+      total_river_blocks += 1
+
+# Now, let's caculate the volume of the river.
+vol_per_block = THICKNESS * (HEIGHT / 25.0) * BLOCK_WIDTH
+total_vol_liters = vol_per_block * total_river_blocks * 0.0163871
+
+print 'Total river volume: ', total_vol_liters
 
 plt.imshow(table, aspect='auto', cmap='binary', interpolation='none',
            vmin=0.0, vmax=1.0)
